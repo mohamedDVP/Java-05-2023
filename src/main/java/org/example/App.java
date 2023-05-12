cf47e6a44069fa45e5b188c66776397b4baf2834
mohamedDVP <39882879+mohamedDVP@users.noreply.github.com>
2023-05-12 23:46:19 +0200

    les types de variables (nombres)
    Signed-off-by: mohamedDVP <39882879+mohamedDVP@users.noreply.github.com>

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
+        // entier
+        byte a = 1; // byte est un nombre entier
+        short b = 1; // short est un nombre entier
+        int c = 1; // int est un nombre entier
+        long l = 1L; // long est un nombre entier long
+
+        // virgule
+        float f = 1.0f; // float est un nombre à virgule
+        double d = 1.0; // double est un nombre à virgule
+        double convertFloat = (double) f; // convertir un float en double
+
+
 
         System.out.println( "Hello World!" );
     }
